ca7e9de304d8b74fad60a1b246deb683954880f1
metahnetoutage <46772070+metahnetoutage@users.noreply.github.com>
2021-02-11 14:48:23 -0800

    Add files via upload
    Added printing capability and both splitting value into list or string.

--- csstyle.js
+++ csstyle.js
@@ -6,6 +6,7 @@ class CSSStyling {
 			this.providedStyle = chowsenStyle.replaceAll("\t","  ").replaceAll("  "," ").trim();
 		}
 	}
+
 	parseStyle() {
 		this.listOfStyles = [];
 		if (typeof this.providedStyle == "string" && this.providedStyle != "") {
@@ -29,7 +30,16 @@ class CSSStyling {
 						prop_Val[0] = prop_Val[0].trim();
 						if(prop_Val.length == 2){
 							prop_Val[1] = prop_Val[1].trim();
-							style[prop_Val[0]] = prop_Val[1];
+							let prop_s = prop_Val[1].split(',');
+							if(prop_s.length == 1){
+								style[prop_Val[0]] = prop_s[0].trim();
+							}
+							else if(prop_s.length > 1){
+								for(let prop = 0; prop < prop_s.length; prop++){
+									prop_s[prop] = prop_s[prop].trim();
+								}
+								style[prop_Val[0]] = prop_s;
+							}
 						}
 						else if(prop_Val.length == 1 && prop_Val[0] != "") {
 							style['o'] = prop_Val[0];
@@ -43,16 +53,56 @@ class CSSStyling {
 		}
 		return this.listOfStyles;
 	}
+
+	toString(){
+		let final = "";
+		let r = this.listOfStyles;
+		for(let i = 0; i < r.length; i++){
+			if(r[i].selector.length == 1){
+				final += r[i].selector[0];
+			}
+			else if(r[i].selector.length > 1){
+				final += r[i].selector.join(', ');
+			}
+			final += '{';
+			for(let s in r[i].selector_style){
+				let style_s = r[i].selector_style[s];
+				if(typeof style_s == "string"){
+					final += `\n\t${ s }:${ r[i].selector_style[s] };\n\t`;
+				}
+				else if(Array.isArray(style_s)){
+					final += `\n\t${ s }:${ r[i].selector_style[s].join(', ') };\n\t`;
+				}
+			}
+			final += '}\n\t';
+		}
+		return final;
+	}
 }
 
 //Testing selector parsing
 
-let testCSS = new CSSStyling(`@mixin transform($property) {
-																							-webkit-transform: $property;
-																							-ms-transform: $property;
-																							transform: $property;
-																						}
-																						.box {
-																							@include transform(rotate(30deg));
-																						}`);
-console.log(testCSS.parseStyle());
+let testCSS = new CSSStyling(`body {
+	background: #f3f3f3;
+	color: #252525;
+	line-height: 1.5;
+	font-family: Georgia, serif;
+	padding: 2rem;
+	}
+
+	h1,
+	h2,
+	h3 {
+	font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue,
+		helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+	line-height: 1.1;
+	font-weight: 900;
+	}`);
+	
+testCSS.parseStyle();
+console.log(testCSS.toString());
+
+/*
+  Output:
+  "body{ background:#f3f3f3; color:#252525; line-height:1.1; font-family:-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif; padding:2rem; font-weight:900; } h1, h2, h3{ background:#f3f3f3; color:#252525; line-height:1.1; font-family:-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif; padding:2rem; font-weight:900; } "
+*/
