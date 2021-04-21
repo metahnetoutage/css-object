340c868e114ba38ce8c0176741160772d98622e6
metahnetoutage <dimametahnet@gmail.com>
2021-04-21 13:11:30 -0700

    major bug fixes finished and seamless experience

--- csstyle.js
+++ csstyle.js
@@ -1,115 +1,329 @@
+/**
+ * Provides a dynamic way to style any DOM element and show difference of styles if one exists
+ * 
+ * * @author Dmitri Dimov brought by CreativeOutage.com
+ */
 class CSSStyling {	
 	constructor(chowsenStyle) {
+		//finalStyles is the end point for storing dynamic styles that will be saved
+		this.finalStyles = this.parseStyle(chowsenStyle);
+	}
+
+	/**
+	 * Parses CSS string into a list of objects that can be dynamically edited
+	 * @param {*} chowsenStyle is a string containing css data to be parsed 
+	*/
+	parseStyle(chowsenStyle) {
 		if ((typeof chowsenStyle == "string" && chowsenStyle != "") ||
 			(typeof chowsenStyle == "object" && chowsenStyle != null) ||
 			chowsenStyle != undefined) {
+			
+			this.listOfStyles = [];
 			this.providedStyle = chowsenStyle.replaceAll("\t","  ").replaceAll("  "," ").trim();
-		}
-	}
-	/*
-	   Parses CSS string into a list of objects that can be dynamically edited
-	*/
-	parseStyle() {
-		this.listOfStyles = [];
-		if (typeof this.providedStyle == "string" && this.providedStyle != "") {
-			while(this.providedStyle.length > 0){
-				let style = {};
-				let startIndex = this.providedStyle.indexOf('{');
-				let selectorPart = this.providedStyle.slice(0, startIndex).trim();
-				let endIndex = this.providedStyle.indexOf('}');
-				let styleBody = this.providedStyle.slice(startIndex + 1, endIndex - 1).trim();
-				let stylePrefs = styleBody.split(';');
-				let selector_sList = selectorPart.split(',');
-				let selector_s = [];
-				for(let selectorS in selector_sList){
-					selector_s.push(selector_sList[selectorS].trim());
-				}
-				for(let stylePref = 0; stylePref < stylePrefs.length; stylePref++){
-					stylePrefs[stylePref] = stylePrefs[stylePref].trim();
-					if(stylePrefs[stylePref] != ""){
-						let prop_Val = stylePrefs[stylePref].split(':');
-						let prop_s = "";
-						prop_Val[0] = prop_Val[0].trim();
-						if(prop_Val.length == 2){
-							prop_Val[1] = prop_Val[1].trim();
-							prop_s = prop_Val[1].split(',');
-							for(let prop = 0; prop < prop_s.length; prop++){
-								prop_s[prop] = prop_s[prop].trim();
-								style[prop_Val[0]] = prop_s;
+			
+			if (typeof this.providedStyle == "string" && this.providedStyle != "" && this.listOfStyles != undefined) {
+				while(this.providedStyle.length > 0){
+					let style = {};
+					let startIndex = this.providedStyle.indexOf('{');
+					let selectorPart = this.providedStyle.slice(0, startIndex).trim();
+					let endIndex = this.providedStyle.indexOf('}');
+					let styleBody = this.providedStyle.slice(startIndex + 1, endIndex - 1).trim();
+					let stylePrefs = styleBody.split(';');
+					let selector_sList = selectorPart.split(',');
+					let selector_s = [];
+					for(let selectorS in selector_sList){
+						selector_s.push(selector_sList[selectorS].trim());
+					}
+					for(let stylePref = 0; stylePref < stylePrefs.length; stylePref++){
+						stylePrefs[stylePref] = stylePrefs[stylePref].trim();
+						if(stylePrefs[stylePref] != ""){
+							let prop_Val = stylePrefs[stylePref].split(':');
+							let prop_s = "";
+							prop_Val[0] = prop_Val[0].trim();
+							if(prop_Val.length == 2){
+								prop_Val[1] = prop_Val[1].trim();
+								if(prop_Val[1].slice(0,3).toLowerCase() != "rgb"){
+									prop_s = prop_Val[1].split(',');
+									for(let prop = 0; prop < prop_s.length; prop++){
+										prop_s[prop] = prop_s[prop].trim();
+										style[prop_Val[0]] = prop_s;
+									}
+								}
+								else {
+									prop_s = prop_Val[1];
+									style[prop_Val[0]] = [ prop_s ];
+								}
+							}
+							else if(prop_Val.length == 1) {
+								style['o'] = prop_Val[0];
 							}
 						}
-						else if(prop_Val.length == 1) {
-							style['o'] = prop_Val[0];
-						}
 					}
+					this.listOfStyles.push({ selector: selector_s, selector_style:style });
+					this.providedStyle = this.providedStyle.slice(endIndex + 1, this.providedStyle.length);
 				}
-				this.listOfStyles.push({ selector: selector_s, selector_style:style });
-				this.providedStyle = this.providedStyle.slice(endIndex + 1, this.providedStyle.length);
 			}
 		}
 		return this.listOfStyles;
 	}
 
-	showIdentified(name){
-		if(typeof name === "string"){
-			for(let i in this.listOfStyles){
-				for(let j in i.selector){
-					if(j == name){
-						return i;
-					}
+	/**
+	 * 
+	 * @param {*} obj is a JSON string that is converted to regular string
+	 * @returns string that parseStyle can understand
+	 */
+	JSONtoString(obj){
+		let objToProps = obj.slice(1,-1);
+		let start = 0;
+		let result = "";
+		for(let i = 0; i < objToProps.length; i++){
+		 	if(objToProps[i] == ',' && (i - 1 > 0 && (objToProps[i - 1] == '%' || objToProps[i - 1] >= '0' && objToProps[i - 1] <= '9'))||
+			   (objToProps[i] >= '0' && objToProps[i] <= '9' && (i - 1 > 0 && objToProps[i - 1] == ','))){
+			}
+			else if(objToProps[i] == ',' || i == objToProps.length - 1){
+				if(start == 0){
+					result += `\t${ objToProps.slice(start, i).replaceAll("\"","") };`;
+				}
+				else {
+					result += `\n\t${ objToProps.slice(start + 1, i).replaceAll("\"","") };`;
+				}
+				start = i;
+			}
+		}
+		return `{\n${ result }\n}`;
+	}
+
+	/**
+	 * 
+	 * @param {*} name is the name of selector or group of selectors that must display json object 
+	 * @returns JSON object corresponding to name
+	 */
+	showStyle(name){
+		if(typeof name === "string" && this.finalStyles != undefined){
+			for(let i in this.finalStyles){
+				if(name == this.finalStyles[i].selector.join(', ')){
+					return this.finalStyles[i];
 				}
 			}
 		}
+		return;
 	}
 
-	updateSelector(name, obj){
-		if(typeof name === "string" && obj != null && typeof obj == "object"){
+	
+	/**
+	 * 
+	 * @param {*} obj1 JSON style object to change 
+	 * @param {*} obj2 JSON style object to change to
+	 * @returns JSON object which specifies if difference has been made
+	 */
+	displayDifference(obj1, obj2){
+		let showDifference = "\n\t";
+		if(typeof obj1 != "undefined" && typeof obj2 != "undefined" && obj1 != undefined && obj2 != undefined && obj1 != null && obj2 != null){
+			if(Object.keys(obj1).length >= Object.keys(obj2[0].selector_style).length){
+				let showDifference = "\n\t";
+				for(let i in obj1){
+					for(let j in obj2[0].selector_style){
+						if(i == j){
+							if(obj1[i].length == obj2[0].selector_style[j].length){
+								if(obj1[i].length == 1){
+									if(obj1[i][0] != obj2[0].selector_style[j][0]){
+										showDifference += `${ i }:${ obj1[i] } -> ${ j }:${ obj2[0].selector_style[j] }\n\t`;
+									}
+								}
+								else if(obj1[i].length > 1){
+									for(let k in obj2[0].selector_style[j]){
+										if(obj1[i][k] != obj2[0].selector_style[j][k])
+										{
+											showDifference += `${ i }:${ obj1[i][k] } -> ${ j }:${ obj2[0].selector_style[j][k] }\n\t`;
+										}
+									}
+								}
+							}
+							else if(obj1[i].length < obj2[0].selector_style[j].length ||
+									obj1[i].length > obj2[0].selector_style[j].length){
+										showDifference += `${ i }:${ obj1[i][k] } -> ${ j }:${ obj2[0].selector_style[j][k] }\n\t`;
+							}
+						}
+					}
+				}
+				return { isDiff: (showDifference == "\n\t")?false:true, styleDiff:showDifference };
+			}
+			else if(Object.keys(obj1).length < Object.keys(obj2[0].selector_style).length){
+				let showDifference = "\n\t";
+				for(let i in obj2[0].selector_style){
+					for(let j in obj1){
+						if(i == j){
+							if(obj2[i].length == obj1[0].selector_style[j].length){
+								if(obj2[i].length == 1){
+									if(obj2[i][0] != obj1[0].selector_style[j][0]){
+										showDifference += `${ i }:${ obj2[i] } -> ${ j }:${ obj1[0].selector_style[j] }\n\t`;
+									}
+								}
+								else if(obj2[i].length > 1){
+									for(let k in obj1[0].selector_style[j]){
+										if(obj2[i][k] != obj1[0].selector_style[j][k])
+										{
+											showDifference += `${ i }:${ obj2[i][k] } -> ${ j }:${ obj1[0].selector_style[j][k] }\n\t`;
+										}
+									}
+								}
+							}
+							else if(obj2[i].length < obj1[0].selector_style[j].length ||
+									obj2[i].length > obj1[0].selector_style[j].length){
+										showDifference += `${ i }:${ obj2[i][k] } -> ${ j }:${ obj1[0].selector_style[j][k] }\n\t`;
+							}
+						}
+					}
+				}
+				return { isDiff: (showDifference == "\n\t")?false:true, styleDiff:showDifference };
+			}
+		}
+		return { isDiff: false, styleDiff:"nothing done!" };
+	}
+
+	/**
+	 * 
+	 * @param {*} name name of selector or group of selectors 
+	 * @param {*} obj json object to update to given selector name
+	 * @param {*} showMessage console log shows success message, default is true, may be set to false
+	 * @param {*} showDiff console log shows difference and is set to true by default, may be set to false
+	 * @returns nothing
+	 */
+	 updateSelector(name, obj, showMessage = true, showDiff = true){
+		if(typeof name === "string" && obj != null && typeof obj == "string" && this.finalStyles != undefined){
 			let message = "";
-			for(let i = 0; i < this.listOfStyles.length; i++){
-				let foundSelector = this.listOfStyles[i].selector.join(', ');
+			let objReceived = this.parseStyle(`${ name } ${ this.JSONtoString(obj) }`);
+			
+			for(let i = 0; i < this.finalStyles.length; i++){
+				let foundSelector = this.finalStyles[i].selector.join(', ');
 				if(name == foundSelector){
-					this.listOfStyles[i] = obj;
+					if(typeof showDiff == "boolean" && showDiff == true){
+						console.log(this.displayDifference(this.finalStyles[i].selector_style, objReceived).styleDiff);
+					}
+					this.finalStyles.splice(i, 1, { selector: [ name ], selector_style:objReceived[0].selector_style });
 					message = `Successfully applied Styling to ${ foundSelector }`;
 				}
 			}
 			if(message == ""){
 				message = `Failed applying Styling, ${ name } could not be found!`;
 			}
-			console.log(message);
+			if(typeof showMessage == "boolean" && showMessage == true){
+				console.log(message);
+			}
 		}
+		return;
 	}
 
-	/*
+	/**
+	 * 
+	 * @param {*} name Name of css selector to update 
+	 * @param {*} obj JSON style object to update JSON object under name
+	 * @param {*} showMessage console log message for successful update
+	 * @param {*} showDiff console log message showing difference before update was made 
+	 * @returns no value is returned
+	 */
+	updatePartOfSelector(name, obj, showMessage = true, showDiff = true){
+		if(typeof name === "string" && obj != null && typeof obj == "string" && this.finalStyles != undefined){
+			let message = "";
+			let objReceived = this.parseStyle(`${ name } ${ this.JSONtoString(obj) }`);
+			
+			for(let i = 0; i < this.finalStyles.length; i++){
+				let selector = this.finalStyles[i].selector.join(', ');
+				if(selector.includes(name) && selector.length > 1){
+					if(typeof showDiff == "boolean" && showDiff == true){
+						console.log(this.displayDifference(this.finalStyles[i].selector_style, objReceived).styleDiff);
+					}
+					this.finalStyles[i].selector.splice(this.finalStyles[i].selector.indexOf(name), 1);
+					this.finalStyles.splice(i, 0, { selector: [ name ], selector_style:objReceived[0].selector_style });
+					message = `Successfully added ${ name } by applied Styling to ${ selector }`;
+				}
+			}
+			if(message == ""){
+				message = `Failed applying Styling, ${ name } could not be found!`;
+			}
+			if(typeof showMessage == "boolean" && showMessage == true){
+				console.log(message);
+			}
+		}
+		return;
+	}
+
+	/**
+	 * 
+	 * @param {*} name name of selector or part of it
+	 * @param {*} obj string representing property to change
+	 * @param {*} showMessage optionally lets hide the message, defaults to showing success message
+	 * @returns nothing is returned
+	 */
+	updateSelectorRule(name, obj, showMessage = true){
+		if(typeof name === "string" && obj != null && typeof obj == "string" && this.finalStyles != undefined){
+			let message = "";
+			let objReceived = this.JSONtoString(obj).slice(1,-1).split(';');
+			for(let i = 0; i < objReceived.length; i++){
+				if(objReceived[i] == '\n' || objReceived[i] == '\t' || objReceived[i] == '\r' || objReceived[i] == ''){
+					objReceived.splice(i,1);
+					i--;
+				}
+			}
+			if(objReceived.length == 1){
+				objReceived[0] = objReceived[0].replaceAll('\n','').replaceAll('\t','').replaceAll('\r','');
+				objReceived = objReceived[0].split(':');
+			}
+			for(let i = 0; i < this.finalStyles.length; i++){
+				let selector = this.finalStyles[i].selector.join(', ');
+				if(selector.includes(name) || selector == name){		
+					this.finalStyles[i].selector_style[objReceived[0]] = objReceived[1];
+					message = `Successfully updated property ${ objReceived[0] } for ${ name }`;
+				}
+			}
+			if(message == ""){
+				message = `Failed applying property ${ objReceived[0] } for ${ name }!`;
+			}
+			if(typeof showMessage == "boolean" && showMessage == true){
+				console.log(message);
+			}
+		}
+		return;
+	}
+
+	/**
 		Prints a professional string of CSS
+		This method takes no parameters and returns a string for entire css file.
+		@param {*} finalStyles is the parameter that determines smooth work.
+		@returns No value returned if finalStyles is undefined
 	*/
 	toString(){
-		let final = "";
-		let r = this.listOfStyles;
-		
-		for(let i = 0; i < r.length; i++){
-			if(r[i].selector.length == 1){
-				final += r[i].selector[0];
-			}
-			else if(r[i].selector.length > 1){
-				final += r[i].selector.join(', ');
-			}
-			final += '{';
-			for(let s in r[i].selector_style){
-				let style_s = r[i].selector_style[s];
-				if(typeof style_s == "string" && s != "o"){
-					if(s != "o"){
-						final += `\n\t${ s }:${ r[i].selector_style[s] };\n\t`;
+		let final = "\t";
+		if(this.finalStyles != undefined){
+			let r = this.finalStyles;
+			for(let i = 0; i < r.length; i++){
+				if(r[i].selector.length == 1){
+					final += r[i].selector[0];
+				}
+				else if(r[i].selector.length > 1){
+					final += r[i].selector.join(', ');
+				}
+				final += '{';
+				for(let s in r[i].selector_style){
+					let style_s = r[i].selector_style[s];
+					if(typeof style_s == "string" && s != "o"){
+						if(s != "o"){
+							final += `\n\t\t${ s }:${ r[i].selector_style[s] };\t`;
+						}
+						else if(s == "o"){
+							final += `\n\t\t${ r[i].selector_style[s] };\t`;
+						}
 					}
-					else if(s == "o"){
-						final += `\n\t${ r[i].selector_style[s] };\n\t`;
+					else if(Array.isArray(style_s)){
+						final += `\n\t\t${ s }:${ r[i].selector_style[s].join(', ') };\t`;
 					}
 				}
-				else if(Array.isArray(style_s)){
-					final += `\n\t${ s }:${ r[i].selector_style[s].join(', ') };\n\t`;
-				}
+				final += '\n\t}\n\t\n\t';
 			}
-			final += '}\n\t';
+			return final;
 		}
-		return final;
+		return;
 	}
 }
+
+export { CSSStyling };
---
+++ testing.js
@@ -0,0 +1,218 @@
+import { CSSStyling } from './csstyle.js';
+
+/*
+    Testing or unit testing can happen below
+*/
+
+let styling = new CSSStyling(`#content_settings {
+                                position: absolute;
+                                width:458px;
+                                height:400px;
+                                margin: 8px 8px auto calc(5% - 8px);
+                                border-width: 1px;
+                                border-color: black;
+                                border-style: solid;
+                                border-radius:5px;
+                            }
+
+                            #close {
+                                width:15px;
+                                height:15px;
+                                margin: 2px 50% 2px calc(50% + 1.5px);
+                                border-width: 1px;
+                                border-color: black;
+                                background-color:rgb(41, 39, 39);
+                                color: lightgrey;
+                                border-style: solid;
+                                border-radius: 100px;
+                                cursor: default;
+                                display:block;
+                            }
+
+                            #close:hover {
+                                background-color: lightgrey;
+                                color: rgb(41, 39, 39);
+                            }
+
+                            #close div {
+                                width:7.5px;
+                                height:12px;
+                                line-height:12px;
+                                margin: 1px auto auto 22%;
+                                font-family: monospace;
+                                font-weight: bolder;
+                                font-size: 12px;
+                                font-style: italic;
+                            }
+
+                            #styling {
+                                position: absolute;
+                                border-width: 1px;
+                                border-color: rgb(44, 194, 56);
+                                border-style: solid;
+                                width:438px;
+                                height: 359px;
+                                border-radius: 5px;
+                                margin: 12.5px 9px auto 9px; 
+                            }
+
+                            #styling_components {
+                                position: absolute;
+                                border-width: 1px;
+                                border-color: rgb(196, 55, 55);
+                                border-style: solid;
+                                border-radius:5px;
+                                width:224px;
+                                height:354px;
+                                margin: 2px auto auto 4px;
+                                display:inline-block;
+                            }
+
+                            #title_1 {
+                                position: relative;
+                                border-radius:5px;
+                                width:218px;
+                                height:20px;
+                                margin: 5px auto auto 3px;
+                                background:rgb(58, 53, 53);
+                                display:block;
+                            }
+
+                            #title_1 div {
+                                text-align: center;
+                                background-color: rgba(255,255,255,0.7);
+                                height:20px;
+                                border-style: none;
+                            }
+
+                            #styles {
+                                position: relative;
+                                border-width: 1px;
+                                border-color: orange;
+                                border-style: solid;
+                                border-radius: 5px;
+                                height: 321px;
+                                width: 217px;
+                                margin:4px 2px auto 2px;
+                            }
+
+                            #extras {
+                                position: absolute;
+                                border-width: 1px;
+                                border-color: rgb(196, 55, 55);
+                                border-style: solid;
+                                border-radius:5px;
+                                width:198px;
+                                height:354px;
+                                margin: 2px 4px auto 233px;
+                                display:inline-block;
+                            }
+
+                            #title_2 {
+                                position: relative;
+                                border-radius:5px;
+                                width:192px;
+                                height:20px;
+                                margin: 5px auto auto 3px;
+                                background:rgb(58, 53, 53);
+                                display:block;
+                            }
+
+                            #title_2 div {
+                                text-align: center;
+                                background-color: rgba(255,255,255,0.7);
+                                height:20px;
+                                border-style: none;
+                            }
+
+                            #units {
+                                position: relative;
+                                border-width: 1px;
+                                border-color: orange;
+                                border-style: solid;
+                                border-radius: 5px;
+                                height: 321px;
+                                width: 191px;
+                                margin:4px 2px auto 2px;
+                            }
+
+                            .options {
+                                position: relative;
+                                border-radius: 5px;
+                                height:321px;
+                                width:200px;
+                                background-color: rgb(77, 75, 75);
+                                display:inline-block;
+                            }
+
+                            .miniDataDiv_ {
+                                position: absolute;
+                                width:198px;
+                                height:321px;
+                                border-color:transparent;
+                                border-width:1px;
+                                border-style:solid;
+                                border-radius:5px;
+                            }
+
+                            .miniDataDiv_2, h3 {
+                                position: absolute;
+                                width:172px;
+                                height:319px;
+                                border-color:transparent;
+                                border-width:1px;
+                                border-style:solid;
+                                border-radius:5px;
+                            }
+
+                            .options2 {
+                                position: relative;
+                                border-radius: 5px;
+                                height:321px;
+                                width:174px;
+                                background-color: rgb(77, 75, 75);
+                                display:inline-block;
+                            }`);
+
+
+console.log(styling.toString());
+
+console.log('\n---------------------------------------------------\n');
+
+console.log('show style');
+console.log(styling.showStyle('.miniDataDiv_2, h3')); //works well
+
+console.log('updated .options2\n');
+styling.updateSelector('.options2', JSON.stringify({
+                                    'position': 'relative',
+                                    'border-radius': '6px',
+                                    'height':'321px',
+                                    'width':'174px',
+                                    'background-color': 'rgb(77, 75, 75)',
+                                    'display':'inline-block'
+                                    }));
+console.log(styling.toString());
+
+console.log('\n---------------------------------------------------\n');
+
+console.log('updated h3\n');
+styling.updatePartOfSelector('h3',JSON.stringify({
+                                    'position': 'absolute',
+                                    'width':'172px',
+                                    'height':'319px',
+                                    'border-color':'transparent',
+                                    'border-width':'1px',
+                                    'border-style':'solid',
+                                    'border-radius':'6px'
+                                    }));
+
+console.log(styling.toString());
+
+console.log('\n---------------------------------------------------\n');
+
+console.log('updated h3\n');
+styling.updateSelectorRule('h3',JSON.stringify({
+                                    'width':'200px'
+                                    }));
+
+console.log(styling.toString());
